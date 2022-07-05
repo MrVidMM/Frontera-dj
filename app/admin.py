d6312b6f6914a9b3bd283fa4c266dea008190bfe
VidMM <104316628+ElVidMM@users.noreply.github.com>
2022-07-05 01:43:49 -0400

    Opciones de agregado en visual Admin

--- app/admin.py
+++ app/admin.py
@@ -7,6 +7,21 @@ class ProductoAdmin(admin.ModelAdmin):
     search_fields = ['codigo']
     list_per_page = 10
 
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['run','nombre','correo','numero','tipo','created_at','updated_at']
+    search_fields = ['run']
+    list_per_page = 10
+
+class SuscripcionAdmin(admin.ModelAdmin):
+    list_display = ['usuario_sus','estado_sus']
+    search_fields = ['usuario_sus']
+    list_per_page = 10
+
+class CarritoAdmin(admin.ModelAdmin):
+    list_display = ['id','producto','cantidad','usuario']
+    search_fields = ['usuario']
+    list_per_page = 10
+
 admin.site.register(TipoProducto)
 admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Carrito)
